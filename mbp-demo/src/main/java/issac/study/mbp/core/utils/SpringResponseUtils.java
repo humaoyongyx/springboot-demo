21ef6c91fcbb9d21166acfb64451932a8c1459e1
issac.hu <humy6@shanghai-electric.com>
2020-12-25 15:24:26 +0800

    代码优化

--- mbp-demo/src/main/java/issac/study/mbp/core/utils/SpringResponseUtils.java
+++ mbp-demo/src/main/java/issac/study/mbp/core/utils/SpringResponseUtils.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URLEncoder;
 
 /**
@@ -24,22 +21,42 @@ public class SpringResponseUtils {
      * @return
      */
     public static ResponseEntity<InputStreamResource> writeFileToResponse(File file) throws FileNotFoundException {
+        long contentLength = file.length();
+        String fileName = file.getName();
+        return writeFileToResponse(new FileInputStream(file), fileName, contentLength);
+    }
+
+    /**
+     * 输出文件到浏览器
+     *
+     * @param inputStream 输入流
+     * @param fileName    输出文件名称
+     * @return
+     * @throws IOException
+     */
+    public static ResponseEntity<InputStreamResource> writeFileToResponse(InputStream inputStream, String fileName) throws IOException {
+        long contentLength = inputStream.available();
+        return writeFileToResponse(inputStream, fileName, contentLength);
+    }
+
+    private static ResponseEntity<InputStreamResource> writeFileToResponse(InputStream inputStream, String fileName, long contentLength) {
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
         HttpHeaders headers = new HttpHeaders();
         //设置客户端不要缓存
         headers.add(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
         headers.add(HttpHeaders.PRAGMA, "no-cache");
         headers.add(HttpHeaders.EXPIRES, "0");
-        String encodeFileName = null; //
+        String encodeFileName = null;
         try {
-            encodeFileName = URLEncoder.encode(file.getName(), "UTF-8");
+
+            encodeFileName = URLEncoder.encode(fileName, "UTF-8");// 编码解决乱码问题
         } catch (UnsupportedEncodingException e) {
             //ignore
         }
         //https://tools.ietf.org/html/rfc6266 协议规范
+        //headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename*=utf-8''" + encodeFileName);
         //兼容 RFC 5987
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + encodeFileName + ";filename*=utf-8''" + encodeFileName);
-        //headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename*=utf-8''" + encodeFileName);
-        InputStreamResource inputStreamResource = new InputStreamResource(new FileInputStream(file));
-        return ResponseEntity.ok().headers(headers).contentLength(file.length()).contentType(MediaType.parseMediaType("application/octet-stream")).body(inputStreamResource);
+        return ResponseEntity.ok().headers(headers).contentLength(contentLength).contentType(MediaType.parseMediaType("application/octet-stream")).body(inputStreamResource);
     }
 }
