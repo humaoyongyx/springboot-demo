0a5f718f7193b675522235ad98be52c72487334f
issac.hu <humy6@shanghai-electric.com>
2020-12-30 15:15:06 +0800

    @EnableScheduling

--- mbp-demo/src/main/java/issac/study/mbp/MbpApplication.java
+++ mbp-demo/src/main/java/issac/study/mbp/MbpApplication.java
@@ -2,8 +2,10 @@ package issac.study.mbp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MbpApplication {
 
     public static void main(String[] args) {
