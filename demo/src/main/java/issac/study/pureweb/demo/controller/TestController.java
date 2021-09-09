a0438f3db32cbb35e5f06323b6c459d56687229a
issac.hu <humaoyongyx@163.com>
2021-09-09 15:43:08 +0800

    ziputils support response

--- demo/src/main/java/issac/study/pureweb/demo/controller/TestController.java
+++ demo/src/main/java/issac/study/pureweb/demo/controller/TestController.java
@@ -1,9 +1,12 @@
 package issac.study.pureweb.demo.controller;
 
+import issac.study.pureweb.demo.utils.SpringResponseUtils;
+import issac.study.pureweb.demo.utils.ZipUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,4 +25,11 @@ public class TestController {
         map.put("test", 1);
         return map;
     }
+
+    @GetMapping("/2")
+    public void test2(HttpServletResponse response) throws Exception {
+        SpringResponseUtils.setResponse(response, "测试.zip");
+        ZipUtils.doZip("D:\\release", response.getOutputStream());
+    }
+
 }
---
+++ demo/src/main/java/issac/study/pureweb/demo/utils/SpringResponseUtils.java
@@ -0,0 +1,77 @@
+package issac.study.pureweb.demo.utils;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
+
+/**
+ * spring输出文件流工具类
+ *
+ * @author issac.hu
+ */
+public class SpringResponseUtils {
+    /**
+     * 输出文件到浏览器
+     *
+     * @param file 文件
+     * @return
+     */
+    public static ResponseEntity<InputStreamResource> writeFileToResponse(File file) throws FileNotFoundException {
+        long contentLength = file.length();
+        String fileName = file.getName();
+        return writeFileToResponse(new FileInputStream(file), fileName, contentLength);
+    }
+
+    /**
+     * 输出文件到浏览器
+     *
+     * @param inputStream 输入流
+     * @param fileName    输出文件名称
+     * @return
+     * @throws IOException
+     */
+    public static ResponseEntity<InputStreamResource> writeFileToResponse(InputStream inputStream, String fileName) throws IOException {
+        long contentLength = inputStream.available();
+        return writeFileToResponse(inputStream, fileName, contentLength);
+    }
+
+    private static ResponseEntity<InputStreamResource> writeFileToResponse(InputStream inputStream, String fileName, long contentLength) {
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+        HttpHeaders headers = new HttpHeaders();
+        //设置客户端不要缓存
+        headers.add(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+        headers.add(HttpHeaders.PRAGMA, "no-cache");
+        headers.add(HttpHeaders.EXPIRES, "0");
+        String encodeFileName = null;
+        try {
+
+            encodeFileName = URLEncoder.encode(fileName, "UTF-8");// 编码解决乱码问题
+        } catch (UnsupportedEncodingException e) {
+            //ignore
+        }
+        //https://tools.ietf.org/html/rfc6266 协议规范
+        //headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename*=utf-8''" + encodeFileName);
+        //兼容 RFC 5987
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + encodeFileName + ";filename*=utf-8''" + encodeFileName);
+        return ResponseEntity.ok().headers(headers).contentLength(contentLength).contentType(MediaType.parseMediaType("application/octet-stream")).body(inputStreamResource);
+    }
+
+    public static void setResponse(HttpServletResponse response, String fileName) {
+        response.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+        response.setHeader(HttpHeaders.PRAGMA, "no-cache");
+        response.setHeader(HttpHeaders.EXPIRES, "0");
+        response.setContentType("application/octet-stream");
+        String encodeFileName = null;
+        try {
+            encodeFileName = URLEncoder.encode(fileName, "UTF-8");// 编码解决乱码问题
+        } catch (UnsupportedEncodingException e) {
+            //ignore
+        }
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + encodeFileName + ";filename*=utf-8''" + encodeFileName);
+    }
+}
